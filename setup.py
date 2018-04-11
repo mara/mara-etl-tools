1497edaf865550389d2df44bf488c4348896c6d8
Martin Loetzsch <martin.loetzsch@gmail.com>
2018-04-11 13:04:46 +0200

    Create version 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='etl-tools',
-    version='0.1.0',
+    version='1.0.0',
 
     description='Utilities for creating ETL pipelines with mara',
 
