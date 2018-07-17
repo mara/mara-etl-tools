ca715e1cafae433d640caa65cec7b58389c81fae
Martin Loetzsch <martin.loetzsch@gmail.com>
2018-07-17 23:24:36 +0200

    Also process enum columns in util.create_data_set_attributes_table

--- etl_tools/initialize_utils/data_sets.sql
+++ etl_tools/initialize_utils/data_sets.sql
@@ -3,30 +3,45 @@
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
 
 -- creates a table optimized for the auto-completion of data set attributes
-CREATE OR REPLACE FUNCTION util.create_data_set_attributes_table(schema_name TEXT, table_name TEXT)
+CREATE OR REPLACE FUNCTION util.create_data_set_attributes_table(schema_name_ TEXT, table_name_ TEXT)
   RETURNS VOID AS $$
-DECLARE column_name TEXT;
+DECLARE column_name_ TEXT;
 BEGIN
-  EXECUTE 'DROP TABLE IF EXISTS ' || schema_name || '.' || table_name || '_attributes';
-  EXECUTE 'CREATE TABLE ' || schema_name || '.' || table_name ||
+  EXECUTE 'DROP TABLE IF EXISTS ' || schema_name_ || '.' || table_name_ || '_attributes';
+  EXECUTE 'CREATE TABLE ' || schema_name_ || '.' || table_name_ ||
           '_attributes (attribute TEXT NOT NULL, value TEXT NOT NULL);';
 
-  FOR column_name IN EXECUTE 'SELECT column_name FROM information_schema.columns WHERE table_schema = ''' ||
-                             schema_name || ''' AND table_name   = ''' || table_name ||
-                             ''' AND data_type IN (''text'',''varchar'')' LOOP
-    EXECUTE 'INSERT INTO ' || schema_name || '.' || table_name || '_attributes ' ||
-            'SELECT DISTINCT ''' || column_name || ''', "' || column_name ||
-            '" FROM ' || schema_name || '.' || table_name ||
-            ' WHERE "' || column_name || '" IS NOT NULL ORDER BY "' || column_name || '"';
+  FOR column_name_ IN
+  WITH enums AS (
+      SELECT DISTINCT
+        typname,
+        nspname
+      FROM pg_type
+        JOIN pg_enum ON pg_type.oid = pg_enum.enumtypid
+        JOIN pg_namespace ON pg_type.typnamespace = pg_namespace.oid
+  )
+  SELECT column_name
+  FROM information_schema.columns
+    LEFT JOIN enums ON udt_schema = enums.nspname AND udt_name = enums.typname
+  WHERE table_schema = schema_name_
+        AND table_name = table_name_
+        AND (data_type IN ('text', 'varchar') OR enums.typname IS NOT NULL)
+
+  LOOP
+    EXECUTE 'INSERT INTO ' || schema_name_ || '.' || table_name_ || '_attributes ' ||
+            'SELECT DISTINCT ''' || column_name_ || ''', "' || column_name_ ||
+            '" FROM ' || schema_name_ || '.' || table_name_ ||
+            ' WHERE "' || column_name_ || '" IS NOT NULL ORDER BY "' || column_name_ || '"';
   END LOOP;
 
-  EXECUTE 'CREATE INDEX ' || table_name || '_attributes__attribute ON ' ||
-          schema_name || '.' || table_name || '_attributes (attribute)';
+  EXECUTE 'CREATE INDEX ' || table_name_ || '_attributes__attribute ON ' ||
+          schema_name_ || '.' || table_name_ || '_attributes (attribute)';
 
-  EXECUTE 'CREATE INDEX ' || table_name || '_attributes__value ON ' ||
-          schema_name || '.' || table_name || '_attributes USING GIN (value gin_trgm_ops)';
+  EXECUTE 'CREATE INDEX ' || table_name_ || '_attributes__value ON ' ||
+          schema_name_ || '.' || table_name_ || '_attributes USING GIN (value gin_trgm_ops)';
 END;
-$$ LANGUAGE plpgsql;
+$$
+LANGUAGE plpgsql;
 
 -- the same as util.create_data_set_attributes_table but changed for parallel execution
 -- before
@@ -37,7 +52,8 @@ BEGIN
   EXECUTE 'CREATE TABLE ' || schema_name || '.' || table_name ||
           '_attributes (attribute TEXT NOT NULL, value TEXT NOT NULL);';
 END;
-$$ LANGUAGE plpgsql;
+$$
+LANGUAGE plpgsql;
 
 -- parallel
 CREATE OR REPLACE FUNCTION util.insert_column_values_into_data_set_attributes_table(schema_name TEXT, table_name TEXT,
@@ -49,7 +65,8 @@ BEGIN
           '" FROM ' || schema_name || '.' || table_name ||
           ' WHERE "' || column_name || '" IS NOT NULL ORDER BY "' || column_name || '"';
 END;
-$$ LANGUAGE plpgsql;
+$$
+LANGUAGE plpgsql;
 
 -- after
 CREATE OR REPLACE FUNCTION util.index_data_set_attributes_table(schema_name TEXT, table_name TEXT)
@@ -61,22 +78,25 @@ BEGIN
   EXECUTE 'CREATE INDEX ' || table_name || '_attributes__value ON ' ||
           schema_name || '.' || table_name || '_attributes USING GIN (value gin_trgm_ops)';
 END;
-$$ LANGUAGE plpgsql;
+$$
+LANGUAGE plpgsql;
 
 -- parallized after function
 CREATE OR REPLACE FUNCTION util.get_data_set_attributes(schema_name TEXT, table_name TEXT)
   RETURNS SETOF TEXT AS $$
 BEGIN
-  return QUERY
-  execute 'SELECT DISTINCT attribute FROM ' || schema_name || '.' || table_name || '_attributes';
-END $$ LANGUAGE plpgsql;
+  RETURN QUERY
+  EXECUTE 'SELECT DISTINCT attribute FROM ' || schema_name || '.' || table_name || '_attributes';
+END $$
+LANGUAGE plpgsql;
 
 
 CREATE OR REPLACE FUNCTION util.index_data_set_attributes_table(schema_name TEXT, table_name TEXT, attribute TEXT)
-  RETURNS void AS $$
+  RETURNS VOID AS $$
 BEGIN
   EXECUTE 'CREATE INDEX "' || table_name || '_' || attribute || '" ON ' || schema_name || '.'
           || table_name || '_attributes USING GIN (value gin_trgm_ops) WHERE attribute = ''' || attribute || '''';
 END
-$$ LANGUAGE plpgsql;
+$$
+LANGUAGE plpgsql;
 
