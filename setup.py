1ac4c55f023586fe707a0bef0a448acd500d254d
Martin Loetzsch <martin.loetzsch@gmail.com>
2019-04-13 23:48:05 +0200

    remove dependency_links from setup.py to regain compatibility with recent pip versions

--- setup.py
+++ setup.py
@@ -11,10 +11,6 @@ setup(
         'data_integration>=1.3.0',
     ],
 
-    dependency_links=[
-        'git+https://github.com/mara/data-integration.git@1.0.0#egg=data-integration-1.0.0'
-    ],
-
     packages=find_packages(),
 
     author='Mara contributors',
