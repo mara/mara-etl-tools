b9e173e360c17258285b28d1062e11a718f58f63
Martin Loetzsch <martin.loetzsch@gmail.com>
2019-12-29 23:29:37 +0100

    get function create_enum working on PostgreSQL 12

--- etl_tools/initialize_utils/enums.sql
+++ etl_tools/initialize_utils/enums.sql
@@ -1,7 +1,10 @@
 /** Functions for creating enums from queries */
 
 
-/** Adds a list of strings to an enum type */
+/**
+Adds a list of strings to an enum type
+Deprecated, only needed for PostgreSQL < 12
+*/
 CREATE OR REPLACE FUNCTION util.add_enum_values(type REGTYPE, values_ TEXT [])
   RETURNS VOID AS $$
 DECLARE sortorder INTEGER;
@@ -28,14 +31,33 @@ $$
 LANGUAGE plpgsql;
 
 
+
+
 /** Creates an enum type with a list of values */
 CREATE OR REPLACE FUNCTION util.create_enum(enum_ TEXT, values_ TEXT [])
   RETURNS VOID AS $$
+DECLARE value      TEXT;
+        pg_version INTEGER;
 BEGIN
   EXECUTE 'DROP TYPE IF EXISTS ' || enum_ || ' CASCADE';
   EXECUTE 'CREATE TYPE ' || enum_ || ' AS ENUM ();';
 
-  PERFORM util.add_enum_values(enum_ :: REGTYPE, values_);
+  SELECT current_setting('server_version_num') INTO pg_version;
+
+  -- Since PostgreSQL 12, it is possible to add enum values via ALTER TYPE .. ADD VALUE inside a transaction
+  -- Before version 12, direct inserts into pg_enum were needed
+  IF (pg_version >= 120000) THEN
+      FOR value IN SELECT unnest(values_) AS value
+      LOOP
+         EXECUTE 'ALTER TYPE ' || enum_ || ' ADD VALUE ''' || value || '''';
+      END LOOP;
+  ELSE
+      PERFORM util.add_enum_values(enum_ :: REGTYPE, values_);
+  END IF;
 END;
 $$
 LANGUAGE plpgsql;
+
+
+--SELECT util.create_enum('util.enum_test', array['a', 'b', 'c']);
+--SELECT * from pg_enum;
